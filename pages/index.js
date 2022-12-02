32989f1b64b00e52869ce3870bbe13d7bfec37f9
Jahan Shah <shahjahanrajan@gmail.com>
2022-12-02 22:07:51 +0530

    tailwind config

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,12 @@
-//smart contrac
+import React from 'react';
+
+const Home = () => {
+    return(
+        <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+  
+    )
+}
+
+export default Home;
