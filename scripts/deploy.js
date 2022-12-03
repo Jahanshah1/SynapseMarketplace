b722707ba8d96d3f54159932b756cfe0fdeaa63b
SiddharthaMadhav <gsmadhav653@gmail.com>
2022-12-03 10:58:36 +0530

    smart contracts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,25 +1,16 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
+  const NFT = await ethers.getContractFactory("NFT");
+  const DeployedNFT = NFT.deploy();
+  // await DeployedNFT.deployed();
+  console.log("NFT contract address: ", (await DeployedNFT).address);
+  const MarketPlace = await ethers.getContractFactory("Marketplace");
+  const DeployedMarketPlace = MarketPlace.deploy(1);
+  // await DeployedMarketPlace.deployed();
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    "MarketPlace contract address: ",
+    (await DeployedMarketPlace).address
   );
 }
 
